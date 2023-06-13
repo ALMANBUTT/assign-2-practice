279ab9228995ad8bf61635964b410d03fffdcfaf
Alman Butt <almanbutt6@outlook.com>
2023-06-13 09:21:30 +0500

    multiply.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from add import add
+from subtract import subtarct
+
+#taking input from user
+num1=int(input('enter 1st number: '))
+num2=int(input('enter 2nd number: '))
+result=add(num1,num2)
+print('The Sum of Two Number is: ',result)
+
+
+#taking input from user
+num1=int(input('enter 1st number: '))
+num2=int(input('enter 2nd number: '))
+result=subtarct(num1,num2)
+print('The Subtract of Two Number is: ',result)
